1250de01ce7bc442145660bb9ce7b71c28673c21
60dayss <60015908+60dayss@users.noreply.github.com>
2020-01-21 00:25:41 +0300

    Update short.cpp

--- short.cpp
+++ short.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 int main() {
-    setlocale(0, "");
+    setlocale(LC_ALL, "Russian");
 
-    
+
     cout << " Лопатин Глеб группа номер 9005\n";
     cout << "***\n";
     cout << "f1(x)=-89,42x^8-543,76x^2+32,76x \n";
@@ -20,15 +20,15 @@ int main() {
     cout << "f2_2(x)=x^2(x^2(−8980,032x^2-186,34)-649,32)\n";
     cout << "***\n";
 
-    cout<<"Enter x:  ";
+    cout << "Enter x:  ";
     double x;
     cin >> x;
     //Вычисляем первый полином
-    double polinom1 = x*(x*(-89.42*x*x*x*x*x*x-543.76)+32.76);
+    double polinom1 = x * (x * (-89.42 * x * x * x * x * x * x - 543.76) + 32.76);
     //Вычисляем второй полином
-    double polinom2 = (x*x)*((x*x)*(−8980.032*x*x-186.34)-649.32);
+    double prost;
+    prost = -8980.032;
+    double polinom2 = (x * x) * ((x * x) * (prost * x * x - 186.34) - 649.32);
     //Частное от деления двух полиномов 
-     cout <<"\nРезультатом деления полиномов друг на друга является: "<< setw(4)<< polinom1/polinom2 ;
-    
-
+    cout << "\nРезультатом деления полиномов друг на друга является: " << setw(4) << polinom1 / polinom2;
 }
